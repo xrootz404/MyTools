9ee60cdbfa484ce01f963fd7699a8cf6dd8b5c41
xrootz404 <49157453+xrootz404@users.noreply.github.com>
2019-04-07 00:56:30 +0800

    Add files via upload

---
+++ iamsyamsir.sh
@@ -0,0 +1,263 @@
+bersih
+bi = ' \ 033 [34; 1m '  # biru
+i = ' \ 033 [32; 1m '  # ijo
+pur = ' \ 033 [35; 1m '  # ungu
+cy = ' \ 033 [36; 1m '  # cyan
+saya = ' \ 033 [31; 1m '  # merah
+pu = ' \ 033 [37; 1m '  # ungu
+ku = ' \ 033 [33; 1m '  # kuning
+gema
+gema
+python2 xiaomi5.py
+gema
+bersih
+gema
+echo  $ red "           Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa sayang: v ( "   
+tidur 1
+gema  ' ' ' \ a
+\ 033 [34; 1m _
+\ 033 [34; 1m / |
+\ 033 [34; 1m | |
+\ 033 [34; 1m | |
+\ 033 [34; 1m _ | _ | _ ' ' '
+tidur 1
+pembaruan apt && peningkatan apt
+untuk menginstal nano
+untuk menginstal git
+pkg instal python python2 vim figlet curl
+bersih
+gema
+gema
+gema
+gema
+echo  $ red "          Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa sayang: v "
+tidur 1
+gema  ' ' ' \ a
+\ 033 [32; 1m ____
+\ 033 [32; 1m | ___ \
+\ 033 [32; 1m __) |
+\ 033 [32; 1m / __ /
+\ 033 [32; 1m | _____ | ' ' '
+tidur 1
+untuk memasang php
+Instal pip2 mekanis
+pip2 instal lolcat
+permintaan instalasi pip2
+bersih
+gema
+gema
+gema
+gema
+echo  $ red "          Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa sayang: v "
+tidur 1
+gema  ' ' ' \ a
+\ 033 [35; 1m _____
+\ 033 [35; 1m | ___ /
+\ 033 [35; 1m | _ \
+\ 033 [35; 1m ___) |
+\ 033 [35; 1m | ____ / ' ' '
+tidur 1
+bersih
+gema
+gema
+gema
+gema
+echo  $ red "          Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa sayang: v "
+tidur 1
+gema  ' ' ' \ a
+\ 033 [33; 1m _ _
+\ 033 [33; 1m | || |
+\ 033 [33; 1m | || | _
+\ 033 [33; 1m | __ _ |
+\ 033 [33; 1m | _ | ' ' '
+tidur 1
+bersih
+gema
+gema
+gema
+gema
+echo  $ red "          Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa sayang: v "
+tidur 1
+gema  ' ' ' \ a
+\ 033 [36; 1m ____
+\ 033 [36; 1m | ___ |
+\ 033 [36; 1m | ___ \
+\ 033 [36; 1m ___) |
+\ 033 [36; 1m | ____ / ' ' '
+tidur 1
+bersih
+gema
+echo  $ pur " =================== " $ cy " =================== "
+echo  $ i " Ôûê " $ pu "   AUTHOR: RAHMAD   " $ i "                    Ôûê "
+echo  $ i " Ôûê " $ pu "   WhatsApp: 0887882xxxx " $ i "                 Ôûê "
+echo  $ i " Ôûê " $ pu "   GMAIL: kucingkaget22@gmail.com " $ i "     Ôûê "
+echo  $ i " Ôûê " $ pu "   YOUTUBE: YouTube RMD " $ i "                 Ôûê "
+echo  $ pur " =================== " $ cy " =================== "
+gema
+echo  $ cy " MeNu BRUTEPORCE NEWS: "
+echo  $ ku " [ " $ me " 1 " $ ku " ] " $ i " LAPORAN FB "
+echo  $ ku " [ " $ me " 2 " $ ku " ] " $ i " BRUTE PORCE "
+echo  $ ku " [ " $ me " 3 " $ ku " ] " $ i " BRUTE FORCE FB CRACKER "
+echo  $ ku " [ " $ me " 4 " $ ku " ] " $ i " MBF "
+echo  $ ku " [ " $ me " 5 " $ ku " ] " $ i " CLONING YAHOO "
+echo  $ ku " [ " $ me " 6 " $ ku " ] " $ i " OSIF "
+echo  $ ku " [ " $ me " 7 " $ ku " ] " $ i " PROFILEGUARD FB "
+echo  $ ku " [ " $ me " 8 " $ ku " ] " $ i " BOT REAKSI FACEBOOK "
+echo  $ ku " [ " $ me " 9 " $ ku " ] " $ i " CALL SPAM "
+echo  $ ku " [ " $ me " 10 " $ ku " ] " $ i " SPAM WHATSHAPP "
+echo  $ ku " [ " $ me " 11 " $ ku " ] " $ i " YOUTUBE-DL "
+echo  $ ku " [ " $ me " 12 " $ ku " ] " $ i " LACAK LOKASI "
+echo  $ i " [ " $ me " 13 " $ i " ] " $ ku " EXIT "
+gema
+echo  $ pur " Ôò¡ÔöÇ [ " $ pur " Masukkan pilihan Anda " $ pur " ] "
+baca -p " Ôò¦ÔöÇ >> " pil
+
+if [ $ pil  = 1]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
+git clone https://github.com/IlayTamvan/Report.git
+laporan cd
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $ pil  = 2]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $ pil  = 3]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $ pil  = 4]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $ pil  = 5]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+permintaan instalasi pip2 memekanisasi
+persyaratan pemasangan pip2
+python2 cloning.py
+fi
+
+if [ $ pil  = 6]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 instal -r requirement.txt
+python2 osif.py
+fi
+
+if [ $ pil  = 7]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
+fi
+
+if [ $ pil  = 8]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|Ulolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 2
+echo $ku"Facebook :"$i" IamSyamsir"
+
+
+tidur 2
+echo  $ pur " Terima kasih Yang Sudah Mendukung dengan Tools ini "
+tidur 2
+
+keluar
+fi
